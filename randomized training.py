f219ac5ea694079bcc53e818d0142b14cad3eeed
Omar Cabaleiro <51969062+OmarCabaleiro@users.noreply.github.com>
2023-01-14 17:01:41 -0500

    created 2022 Jun 29

---
+++ randomized training.py
@@ -0,0 +1,20 @@
+import random
+
+movements = ["loaded carry", "squat", "hinge", "superior pull", \
+             "horizontal pull", "inferior pull", "superior push", \
+             "horizontal push", "inferior push", "gait", "jump", "crawl", \
+             "rotation", "trunk extension", "trunk flexion", \
+             "lift from ground", "throwing"]
+
+while True:
+    session = input("Choose: /n 1. 2. 3. c. r.:")
+    if session == "1":
+        print(random.choice(movements))
+    if session == "2":
+        print(random.choices(movements, k=2))
+    if session == "3":
+        print(random.choices(movements, k=3))
+    if session == "c":
+        print(random.choices(movements, k=random.randint(4, 10)))
+    if session == "r":
+        print(random.choices(movements, k=random.randint(1, 10)))
