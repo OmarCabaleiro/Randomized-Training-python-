15d88379dbfb2df274a6ead783656920df0226cd
Omar Cabaleiro <51969062+OmarCabaleiro@users.noreply.github.com>
2023-01-15 19:48:46 -0500

    eliminated repetition of same array element during session

--- randomized training.py
+++ randomized training.py
@@ -6,15 +6,30 @@ movements = ["loaded carry", "squat", "hinge", "superior pull", \
              "rotation", "trunk extension", "trunk flexion", \
              "lift from ground", "throwing"]
 
+##while True:
+##    session = input("Choose: /n 1. 2. 3. c. r.:")
+##    if session == "1":
+##        print(random.choice(movements))
+##    if session == "2":
+##        print(random.choices(movements, k=2))
+##    if session == "3":
+##        print(random.choices(movements, k=3))
+##    if session == "c":
+##        print(random.choices(movements, k=random.randint(4, 10)))
+##    if session == "r":
+##        print(random.choices(movements, k=random.randint(1, 10)))
+
+# trying above, but without repeating elements during single use
 while True:
     session = input("Choose: /n 1. 2. 3. c. r.:")
     if session == "1":
         print(random.choice(movements))
     if session == "2":
-        print(random.choices(movements, k=2))
+        print(random.sample(movements, 2))
     if session == "3":
-        print(random.choices(movements, k=3))
+        print(random.sample(movements, 3))
     if session == "c":
-        print(random.choices(movements, k=random.randint(4, 10)))
+        print(random.sample(movements, k=random.randint(4, 10)))
     if session == "r":
-        print(random.choices(movements, k=random.randint(1, 10)))
+        print(random.sample(movements, k=random.randint(1, 10)))
+
